a02613ad19cc726fb082c9c747d06b86d3893432
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2017-03-23 22:09:09 +0100

    Now loads plugin from running directory.

--- vokram.Core/Utils/Plugins.cs
+++ vokram.Core/Utils/Plugins.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using IrcDotNet.Collections;
@@ -27,11 +28,11 @@ namespace vokram.Core.Utils
             return list;
             */
             var list = new List<IIrcPlugin>();
-            var assembly = Assembly.LoadFile("vokram.Plugins.dll");
+            var assembly = Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "vokram.Plugins.dll"));
             var types = assembly.GetTypes().Where(t => t.Namespace!= null && t.Namespace.Equals("vokram.Plugins") && !t.IsAbstract);
             types.ForEach(t =>
             {
-                var plugin = Activator.CreateInstance(t) as IIrcPlugin;
+                var plugin = assembly.CreateInstance(t.FullName) as IIrcPlugin;
                 list.Add(plugin);
             });
             return list;
