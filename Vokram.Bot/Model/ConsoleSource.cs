ee08a7a8496a9269e66c69a4bbc729657044568e
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2017-04-21 02:56:51 +0200

    Created model directory.

---
+++ Vokram.Bot/Model/ConsoleSource.cs
@@ -0,0 +1,9 @@
+﻿using IrcDotNet;
+
+namespace Vokram.Bot.Model
+{
+    public class ConsoleSource : IIrcMessageSource
+    {
+        public string Name { get; set; } = "Console";
+    }
+}
---
+++ Vokram.Bot/Model/ConsoleTarget.cs
@@ -0,0 +1,10 @@
+﻿using IrcDotNet;
+
+namespace Vokram.Bot.Model
+{
+    public class ConsoleTarget : IIrcMessageTarget
+    {
+        public string Name { get; set; } = "Consoles";
+
+    }
+}
